e66a4f29fdc156f158a40ab0b923187091f1fe90
Helgi Steinarr <helgi203@gmail.com>
2022-05-03 13:52:41 +0000

    updated installed apps

--- fire_sale/settings.py
+++ fire_sale/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin.apps.AdminConfig',
+    'user.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
