e2a3fddf2e59021caa5f0266600833f96c12a42f
ingosibbason <ingolfursibbason@gmail.com>
2022-05-03 17:17:38 +0000

    models started

--- user/models.py
+++ user/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.translation.trans_real import catalog
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    birthday = models.DateField()
+    is_admin = models.BooleanField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+class Auction(models.Model):
+    UID = models.ForeignKey(user, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    price = models.IntegerField()
+    locID =
+    catID = models.ForeignKey(Cetegory, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+
+
