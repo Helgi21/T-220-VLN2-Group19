f7ba5d08147a0d7c63ad68eee8dff73f699c1458
Helgi Steinarr <helgi203@gmail.com>
2022-05-06 14:27:31 +0000

    url fixes

--- auction/urls.py
+++ auction/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.AllAuctions.as_view(), name="auctions"),
-    path('<int:pk>', views.SingleAuction.as_view(), name="single_auction"),
-    path('add_auction', views.AddAuction.as_view(), name="add_auction"),
-    path('offers', views.ViewOffers.as_view(), name="offers"),
+    path('<int:pk>/', views.SingleAuction.as_view(), name="single_auction"),
+    path('add_auction/', views.AddAuction.as_view(), name="add_auction"),
+    path('offers/', views.ViewOffers.as_view(), name="offers"),
 ]
